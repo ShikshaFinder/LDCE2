473ab29a3df4cc188c2071e985df7072191ae948
ShikshaFinder <janiharsh794@gmail.com>
2024-03-13 02:12:07 +0530

    news

--- src/app/data.js
+++ src/app/data.js
@@ -1,42 +1,40 @@
 export const projects = [
   {
-    title: "Matthias Leidinger",
+    title: "LDCE",
     description:
-      "Originally hailing from Austria, Berlin-based photographer Matthias Leindinger is a young creative brimming with talent and ideas.",
+      "LDCE is good college.",
     src: "5.png",
-    link: "https://www.ignant.com/2023/03/25/ad2186-matthias-leidingers-photographic-exploration-of-awe-and-wonder/",
+    link: "ldce.ac.in",
     color: "#BBACAF",
   },
   {
-    title: "Clément Chapillon",
+    title: "LDCE",
     description:
-      "This is a story on the border between reality and imaginary, about the contradictory feelings that the insularity of a rocky, arid, and wild territory provokes”—so French photographer Clément Chapillon describes his latest highly captivating project Les rochers fauves (French for ‘The tawny rocks’).",
+      "LDCE college is good.",
     src: "4.jpg",
-    link: "https://www.ignant.com/2022/09/30/clement-chapillon-questions-geographical-and-mental-isolation-with-les-rochers-fauves/",
+    link: "ldce.ac.in",
     color: "#977F6D",
   },
   {
-    title: "Zissou",
+    title: "LDCE",
     description:
-      "Though he views photography as a medium for storytelling, Zissou’s images don’t insist on a narrative. Both crisp and ethereal, they’re encoded with an ambiguity—a certain tension—that lets the viewer find their own story within them.",
+      "LDCE college is good",
     src: "3.jpeg",
-    link: "https://www.ignant.com/2023/10/28/capturing-balis-many-faces-zissou-documents-the-sacred-and-the-mundane-of-a-fragile-island/",
+    link: "ldce.ac.in",
     color: "#C2491D",
   },
   {
-    title: "Mathias Svold and Ulrik Hasemann",
-    description:
-      "The coastlines of Denmark are documented in tonal colors in a pensive new series by Danish photographers Ulrik Hasemann and Mathias Svold; an ongoing project investigating how humans interact with and disrupt the Danish coast.",
+    title: "LDCE",
+    description: "LDCE college is good",
     src: "2.jpeg",
-    link: "https://www.ignant.com/2019/03/13/a-photographic-series-depicting-the-uncertain-future-of-denmarks-treasured-coastlines/",
+    link: "ldce.ac.in",
     color: "#B62429",
   },
   {
-    title: "Mark Rammers",
-    description:
-      "Dutch photographer Mark Rammers has shared with IGNANT the first chapter of his latest photographic project, ‘all over again’—captured while in residency at Hektor, an old farm in Los Valles, Lanzarote. Titled ‘Beginnings’, the mesmerizing collection of images is a visual and meditative journey into the origins of regrets and the uncertainty of stepping into new unknowns.",
+    title: "LDCE",
+    description: "LDCE college is good",
     src: "1.png",
-    link: "https://www.ignant.com/2023/04/12/mark-rammers-all-over-again-is-a-study-of-regret-and-the-willingness-to-move-forward/",
+    link: "ldce.ac.in",
     color: "#88A28D",
   },
 ];
